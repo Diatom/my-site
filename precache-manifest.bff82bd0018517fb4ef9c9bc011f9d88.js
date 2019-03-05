5919b672f6438664507066eb2678a33480c2195a
Severin <supriore@gmail.com>
2019-03-05 14:29:13 +0300

    Updates

---
+++ precache-manifest.bff82bd0018517fb4ef9c9bc011f9d88.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "be39c8143d750ce79bfb94cc0cde901f",
+    "url": "/my-site/static/media/severin.be39c814.jpg"
+  },
+  {
+    "revision": "620cef5f7c088b605671f5761a09a775",
+    "url": "/my-site/static/media/pasta.620cef5f.jpg"
+  },
+  {
+    "revision": "f7b85df66fe3a31e137aa80db5c3ed76",
+    "url": "/my-site/static/media/circuit.f7b85df6.svg"
+  },
+  {
+    "revision": "240da6d8449537321ecc15240b1f0fa9",
+    "url": "/my-site/static/media/cheddar.240da6d8.jpg"
+  },
+  {
+    "revision": "aad2eaf79a9490f7ff94",
+    "url": "/my-site/static/js/runtime~main.aad2eaf7.js"
+  },
+  {
+    "revision": "4f6e781a0fb6138591eb",
+    "url": "/my-site/static/js/main.4f6e781a.chunk.js"
+  },
+  {
+    "revision": "dbffc4b6d7b48d81a9eb",
+    "url": "/my-site/static/js/2.dbffc4b6.chunk.js"
+  },
+  {
+    "revision": "4f6e781a0fb6138591eb",
+    "url": "/my-site/static/css/main.e1fb258c.chunk.css"
+  },
+  {
+    "revision": "5a097ac7c65c4bde3c8958e74bbaaa9e",
+    "url": "/my-site/index.html"
+  }
+];
